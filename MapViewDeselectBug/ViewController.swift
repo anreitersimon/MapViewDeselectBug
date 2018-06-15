d1e468e7ac60058940b7cd5996f80ed94cb3f58d
simonbility <simon.anreiter@wegfinder.at>
2018-06-15 20:38:50 +0200

    Initial Commit

---
+++ MapViewDeselectBug/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MapViewDeselectBug
+//
+//  Created by Simon Anreiter on 15.06.18.
+//  Copyright © 2018 imobility. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
